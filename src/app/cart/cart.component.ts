c9ff487a161278830f75721cb77d35678da8fe9e
Bin-Ostrowski <bin.ostrowski@gmail.com>
2023-02-13 11:57:22 -0800

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+//This service provides convenient methods for generating controls.
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -11,8 +14,25 @@ export class CartComponent {
   //sets the items using the CartService getItems() method
   items = this.cartService.getItems();
 
+  //To gather the user's name and address, use the FormBuilder group() method to set the checkoutForm property to a form model containing name and address fields.
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
+
   //Inject the CartService so that the CartComponent can use it
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    //Inject the FormBuilder service in the CartComponent constructor(). This service is part of the ReactiveFormsModule module, which you've already imported
+    private formBuilder: FormBuilder
   ) { }
+
+  //Define onSubmit() method to process form. method allows users to submit name and address. also, this method uses clearCart() in CartService to reset the form and clear the cart.
+  onSubmit(): void {
+    // process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
